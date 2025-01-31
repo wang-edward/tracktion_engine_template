794fbd467412286c3f36eedc4bd8e02d5892c71c
Edward Wang <edawordwang@gmail.com>
2025-01-31 01:13:47 -0500

    runsh

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OS="$(uname)"
+
+if [[ "$OS" == "Darwin" ]]; then
+    # macOS
+    BIN_PATH="./build/Gui_artefacts/Gui.app/Contents/MacOS/Gui"
+elif [[ "$OS" == "Linux" ]]; then
+#     BIN_PATH="./build/Box_artefacts/Debug/Box"
+# else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Debug -B build
+
+if [[ $? == 0 ]]; then
+    make -C build -j6
+else
+    echo "cmake failed"
+    exit 1
+fi
+
+if [[ $? == 0 ]]; then
+    $BIN_PATH
+else
+    echo "make failed"
+    exit 1
+fi
